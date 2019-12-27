7eafd7a461ffa17dad11e3d236a15b5007a10586
sarathsky <sarathchandra1999@gmail.com>
2019-12-27 10:14:45 +0530

    common testing

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 var http=require('http');
 
-var server=require('ws').Server;
-var wsport=process.env.PORT || 5001;
-var s=new server({port:wsport});
-
-s.on('connection',(ws)=>{
-    ws.on('message',(message)=>{
-        console.log('Received: ' + message);
-        ws.send('hello dear..');
-    });
-
-});
+// var server=require('ws').Server;
+// var wsport=process.env.PORT || 5001;
+// var s=new server({port:wsport});
+//
+// s.on('connection',(ws)=>{
+//     ws.on('message',(message)=>{
+//         console.log('Received: ' + message);
+//         ws.send('hello dear..');
+//     });
+//
+// });
 
 const httpport = process.env.PORT || 3000;
 http.createServer((req,res)=>{
-    s.clients.forEach((client)=>{
-        console.log('Msg sent to browser..');
-        client.send('You got a call..');
-    });
-    res.write('Server has received your msg..');
-    res.end();
+    // s.clients.forEach((client)=>{
+    //     console.log('Msg sent to browser..');
+    //     client.send('You got a call..');
+    // });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Your msg has been received..</h1>');
 }).listen(httpport,()=>{
     console.log('http listing on '+httpport);
 });
