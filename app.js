b389a15cef20436ae1d4d61d1d8a3f4907a570dc
sarathsky <sarathchandra1999@gmail.com>
2020-01-14 02:31:11 +0530

    lol

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 var http=require('http');
 var fs=require('fs');
 
-//const httpport = process.env.PORT || 5002;
-const wsport=process.env.PORT || 5001;
+const port=process.env.PORT || 5002;
 
-// http.createServer((req,res)=>{
-//
-//     if(req.url=='/ws'){
-//         fs.readFile("website/index.html", function (err, data) {
-//             res.writeHead(200, {'Content-Type': 'text/html'});
-//             res.write(data);
-//             res.end();
-//         });
-//         return;
-//     }else if(req.url=='/http'){
-//         s.clients.forEach((client)=>{
-//             console.log('Msg sent to browser..');
-//             client.send('You got a call..');
-//         });
-//     }
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/html');
-//     res.write('<h1>httpport '+httpport+'</h1>');
-//
-//     res.end('<h1>Your msg has been received..</h1>');
-//
-// }).listen(httpport,()=>{
-//     console.log('http listing on '+httpport);
-// });
+var server = http.createServer((req,res)=>{
+
+    if(req.url=='/ws'){
+        fs.readFile("website/index.html", function (err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        });
+        return;
+    }else if(req.url=='/http'){
+        s.clients.forEach((client)=>{
+            console.log('Msg sent to browser..');
+            client.send('You got a call..');
+        });
+    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>httpport '+port+'</h1>');
+
+    res.end('<h1>Your msg has been received..</h1>');
+
+})
+server.listen(port,()=>{
+    console.log('http listing on '+port);
+});
 
 var wsserver=require('ws').Server;
-var s=new wsserver({port:wsport});
+var wss=new wsserver({server:server});
 
-s.on('connection',(ws)=>{
+wss.on('connection',(ws)=>{
     ws.on('message',(message)=>{
         console.log('Received: ' + message);
         ws.send('hello dear..');
