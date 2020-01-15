e3d1571f0a40c5bb8c7018382b0370616940cd61
sarathsky <sarathchandra1999@gmail.com>
2020-01-15 17:39:49 +0530

    lol

--- app.js
+++ app.js
@@ -5,25 +5,12 @@ const port=process.env.PORT || 5002;
 
 var server = http.createServer((req,res)=>{
 
-    if(req.url=='/ws'){
-        fs.readFile("website/index.html", function (err, data) {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            res.end();
-        });
-        return;
-    }else if(req.url=='/http'){
-        s.clients.forEach((client)=>{
-            console.log('Msg sent to browser..');
-            client.send('You got a call..');
-        });
-    }
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h1>httpport '+port+'</h1>');
-
-    res.end('<h1>Your msg has been received..</h1>');
-
+    fs.readFile("website/index.html", function (err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
+    
 })
 server.listen(port,()=>{
     console.log('http listing on '+port);
